3444b809438cfbf834958998b22a0f961532c455
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-08-03 23:38:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Welcome to Beyond Media News User App</h1>
-      <h1>See All News</h1>
+      <h1>See All News here</h1>
     </div>
   );
 }
